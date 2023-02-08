4952c76ffdae51bea13ed1b89432337d649264bf
Emanuele Maria <emanuuuelem@gmail.com>
2023-02-08 10:44:11 -0300

    add icon to hide the balance

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,12 +3,13 @@ import { verificarTipoValor } from '../../fungeng.js'
 import Botao from '../../components/Botao.jsx';
 import { formatToBRL } from "brazilian-values";
 import { ToastContainer, toast } from "react-toastify";
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
 
 function Home() {
     const [saldo, setSaldo] = useState(Number(localStorage.getItem("saldo")) || 0);
     const [valor, setValor] = useState(0);
     const [humor, setHumor] = useState('😭');
-    const [exibirSaldo, setExibirSaldo] = useState(true);
+    const [eyeOpened, setEyeOpened] = useState(true)
 
     function depositar() {
         if (verificarTipoValor(valor) == false) return;
@@ -53,14 +54,13 @@ function Home() {
                 <input value={valor} onChange={e => setValor(e.target.value)} placeholder='Digite o valor: ' />
                 <Botao name='Depositar' funcao={depositar} color='green' />
                 <Botao name='Sacar' funcao={sacar} color='red' />
-                {exibirSaldo && <span>Saldo: {formatToBRL(saldo)}</span>}
-                <Botao
-                    color='black'
-                    name={`${exibirSaldo ? 'Esconder' : 'Exibir'} saldo`}
-                    funcao={() => {
-                        setExibirSaldo(!exibirSaldo);
-                    }}
-                />
+                
+                {eyeOpened && <>Saldo: {formatToBRL(saldo)}</>}
+
+                <button style={{ color: 'black' }} onClick={() => {
+                    setEyeOpened(!eyeOpened)
+                }}>{eyeOpened ? <AiFillEye size={'1.5rem'}/> : <AiFillEyeInvisible size={'1.5rem'}/>}</button>
+                
             </div>
             <ToastContainer />
         </div>
