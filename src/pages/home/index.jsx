540e66875566a101249126e284ca5a8b9348de63
augustosnk12 <augustosnk12@gmail.com>
2023-02-09 09:29:54 -0300

    style modals

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { verificarTipoValor } from '../../fungeng.js';
-import Botao from '../../components/Botao.jsx';
 import { formatToBRL } from 'brazilian-values';
 import { ToastContainer, toast } from 'react-toastify';
 import Modal from 'react-modal';
 
+import { verificarTipoValor } from '../../fungeng';
+import Botao from '../../components/Botao';
+
+import './styles.scss';
+
+const customStyles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)',
+	},
+};
+
 function Home() {
 	const [saldo, setSaldo] = useState(Number(localStorage.getItem('saldo')) || 0);
-	const [valor, setValor] = useState(0);
+	const [valor, setValor] = useState(null);
 	const [humor, setHumor] = useState('😭');
 	const [exibirSaldo, setExibirSaldo] = useState(true);
+	const [modalDepositIsOpen, setModalDepositIsOpen] = React.useState(false);
+	const [modalWithdrawIsOpen, setModalWithdrawIsOpen] = React.useState(false);
 
 	function saldoAlto() {
 		if (saldo >= 1000) {
@@ -21,6 +37,50 @@ function Home() {
 		}
 	}
 
+	let subtitle;
+
+	function deposit() {
+		if (!valor) {
+			toast.error('Entre com um valor para depósito');
+			return;
+		}
+
+		if (verificarTipoValor(valor) == false) return;
+
+		setSaldo(saldo + Number(valor));
+		setValor(null);
+
+		toast.success('Depósito confirmado!');
+		closeModal('deposit');
+	}
+
+	function withdraw() {
+		if (!valor) {
+			toast.error('Entre com um valor para saque');
+			return;
+		}
+
+		if (verificarTipoValor(valor) == false) return;
+
+		if (Number(valor) > saldo) {
+			toast.error('Saldo insuficiente');
+		} else {
+			setSaldo(saldo - Number(valor));
+			setValor(null);
+
+			toast.success('Depósito confirmado!');
+			closeModal('deposit');
+		}
+	}
+
+	function afterOpenModal() {
+		subtitle.style.color = '#f00';
+	}
+
+	function closeModal(modaType) {
+		modaType == 'deposit' ? setModalDepositIsOpen(false) : setModalWithdrawIsOpen(false);
+	}
+
 	useEffect(() => {
 		saldoAlto();
 	}, [saldo]);
@@ -29,84 +89,63 @@ function Home() {
 		localStorage.setItem('saldo', saldo);
 	}, []);
 
-	let subtitle;
-	const [modalIsOpen, setIsOpen] = React.useState(false);
-	function openModalWithdraw() {
-		setIsOpen(true);
-		withdraw();
-	}
-	function openModalDeposit() {
-		setIsOpen(true);
-		deposit();
-	}
-
-	function deposit() {
-		if (verificarTipoValor(valor) == false) return;
-
-		setSaldo(saldo + Number(valor));
-		setValor(0);
-	}
-	function withdraw() {
-		if (verificarTipoValor(valor) == false) return;
-
-		if (Number(valor) > saldo) {
-			toast.error('Saldo insuficiente');
-		} else {
-			setSaldo(saldo - Number(valor));
-			setValor(0);
-		}
-	}
-
-	function afterOpenModal() {
-		subtitle.style.color = '#f00';
-	}
-
-	function closeModal() {
-		setIsOpen(false);
-	}
-	const customStyles = {
-		content: {
-			top: '50%',
-			left: '50%',
-			right: 'auto',
-			bottom: 'auto',
-			marginRight: '-50%',
-			transform: 'translate(-50%, -50%)',
-		},
-	};
-
 	return (
 		<div className='App'>
 			<h1>Meu banco: {humor} </h1>
 			<div className='tela'>
-				<Botao name='Depositar' funcao={openModalDeposit} color='green' />
-				<Botao name='Sacar' funcao={openModalWithdraw} color='red' />
+				<Botao name='Depositar' funcao={() => setModalDepositIsOpen(true)} color='green' />
+				<Botao name='Sacar' funcao={() => setModalWithdrawIsOpen(true)} color='red' />
 
 				<Modal
-					isOpen={modalIsOpen}
+					isOpen={modalDepositIsOpen}
 					onAfterOpen={afterOpenModal}
-					onRequestClose={closeModal}
+					onRequestClose={() => closeModal('deposit')}
 					style={customStyles}
 					contentLabel='Example Modal'
 				>
-					<h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-					<button onClick={closeModal}>close</button>
-					<div>Digite o valor: </div>
-					<input value={valor} onChange={e => setValor(e.target.value)} placeholder='Digite o valor: ' />
-					<button onClick={() => deposit()}>Enviar</button>
+					<div className='content'>
+						<h2>Digite um valor de Depósito</h2>
+						<div>Digite o valor: </div>
+						<div className='content-actions'>
+							<input
+								autoFocus
+								value={valor}
+								onChange={e => setValor(e.target.value)}
+								placeholder='Digite o valor: '
+								type='number'
+							/>
+							<button className='button' onClick={() => deposit()}>
+								Depositar
+							</button>
+							<button onClick={() => closeModal('deposit')}>X</button>
+						</div>
+					</div>
 				</Modal>
+
 				<Modal
-					isOpen={modalIsOpen}
+					isOpen={modalWithdrawIsOpen}
 					onAfterOpen={afterOpenModal}
-					onRequestClose={closeModal}
+					onRequestClose={() => closeModal('withdraw')}
 					style={customStyles}
 					contentLabel='Example Modal'
 				>
-					<h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2>
-					<button onClick={closeModal}>close</button>
-					<div>Digite o valor: </div>
-					<input value={valor} onChange={e => setValor(e.target.value)} placeholder='Digite o valor: ' />
-					<button onClick={() => withdraw()}>Enviar</button>
+					<div className='content'>
+						<h2>Digite um Saque</h2>
+						<div>Digite o valor: </div>
+						<div className='content-actions'>
+							<input
+								autoFocus
+								value={valor}
+								onChange={e => setValor(e.target.value)}
+								placeholder='Digite o valor: '
+								type='number'
+							/>
+							<button className='button' onClick={() => withdraw()}>
+								Sacar
+							</button>
+							<button onClick={() => closeModal('withdraw')}>X</button>
+						</div>
+					</div>
 				</Modal>
 				{exibirSaldo && <span>Saldo: {formatToBRL(saldo)}</span>}
 				<Botao
