2e4ab95a54fe301c4c46029576a66e66c6b76e8b
Emanuele Maria <emanuuuelem@gmail.com>
2023-02-12 23:51:38 -0300

    end verify login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,29 +1,56 @@
 import "./styles.scss";
-import { Link } from "react-router-dom";
-import { clients } from "../../clients";
+import { Link, redirect } from "react-router-dom";
 import { useState } from "react";
 import { toast } from "react-toastify";
 function Login() {
-  const [cpf, setCPF] = useState(0);
-  const [password, setPassword] = useState("");
-  function verifyLogin() {
-    let cpflogin = document.querySelector("#cpf-login");
-    let passwordLogin = document.querySelector("#password-login");
-    setCPF(cpflogin);
-    setPassword(passwordLogin);
-    console.log(cpf);
-    console.log(password);
-  }
+  const [cpfLogin, setCPFLogin] = useState(0);
+  const [passwordLogin, setPasswordLogin] = useState("");
 
-  /* function verifyLogin() {
-    for(let i = 0; i<clients.length; i++){
-        if(clients.cpf==cpf && clients.password == password){
-            toast.success("acesso confirmado")
-        }else{
-            toast.error("login invalido")
-        }
+  const clients = [
+    {
+        nome: "emanuele",
+        cpf: 11111111111,
+        senha:"1234",
+        saldo: 548655,
+        movimentacao: "withdraw",
+        valor: 155
+    },
+    {
+        nome: "augusto",
+        cpf: 22222222222,
+        senha: "5678",
+        saldo: 451515143,
+        movimentacao: "deposit",
+        valor: 455
+    },
+    {
+        nome: "leticia",
+        cpf: 33333333333,
+        senha:"9101",
+        saldo: 4876258,
+        movimentacao: "withdraw",
+        valor: 456
+    },
+    {
+        nome: "jânio",
+        cpf: 55555555555,
+        senha:"9999",
+        saldo: 100000000000000000,
+        movimentacao: "deposit",
+        valor: 1000000000000000000000000
     }
-  } */
+];
+
+  function verifyLogin() {
+    console.log(cpfLogin)
+    console.log()
+    clients.forEach((user)=>{
+        if(user.cpf==Number(cpfLogin) && user.senha==Number(passwordLogin)){
+            window.location.href="http://localhost:3000/"
+        } 
+        
+    }) 
+  }
 
   return (
     <div className="middle" style={{ backgroundColor: "#fff" }}>
@@ -31,13 +58,26 @@ function Login() {
         <h1 className="h1">Login</h1>
       </div>
       <p className="p">Acesse seu banco</p>
-      <form >
-        <input id="cpf-login" type="number" placeholder="Digite seu CPF:" />
-        <input id="password-login" type="password-login" placeholder="Senha:" />
-        <button type="submit" className="login-button">
+      
+        <input
+          value={cpfLogin}
+          onChange={(e) => setCPFLogin(e.target.value)}
+          type="number"
+          placeholder="Digite seu CPF:"
+        />
+        <input
+        value={passwordLogin}
+        onChange={(e) => setPasswordLogin(e.target.value)}
+        type="password" placeholder="Senha:" />
+
+
+        <button 
+        onClick={()=>verifyLogin()}
+        className="login-button">
           Entrar
         </button>
-      </form>
+        
+      
     </div>
   );
 }
